7da897d2b8e294fe119a8d7fd2a2b5ca0101d323
qzscfchh <csfchh@qq.com>
2021-06-22 01:55:46 +0800

    First commit.

---
+++ service/src/main/java/io/github/qzcsfchh/android/servicediscover/JavaService.java
@@ -0,0 +1,15 @@
+package io.github.qzcsfchh.android.servicediscover;
+
+/**
+ * Desc.
+ *
+ * @author huanghao
+ * @version v1.0
+ * @since 2021/6/22 01:45
+ */
+public interface JavaService {
+
+    void sayHi();
+
+    String doJob();
+}
